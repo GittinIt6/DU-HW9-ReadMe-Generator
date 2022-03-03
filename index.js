8de5403465ffbda7a792849f5c55c75a5620d149
GittinIt6 <mjeluso@gmail.com>
2022-03-02 18:53:31 -0700

    updates to index.js for formatting

--- index.js
+++ index.js
@@ -82,31 +82,23 @@ function init() {
         // console.log(response)
         // let names = questions.map(({ name }) => name).join(', ');
         const { title, description, instructions, userType, techKnowledge, technologies, revHistory, contribute, license } = response;
-        let readmeData = `<!DOCTYPE html>
-        ${title}
-        ${description}, 
-            ${instructions}, 
-                ${userType}, 
-                    ${techKnowledge}, 
-                        ${technologies}, 
-                            ${revHistory}, 
-                                ${contribute}, 
-                                    ${license}
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>week 9 mini project</title>
-        </head>
-        <body>
-        Name: {username} <br>
-        Location: {location} <br>
-        Bio: {bio} <br>
-        <a href="{linkedin}">LinkedIn URL</a> <br>
-        <a href="{github}">GitHub URL</a> <br>
-        </body>
-        </html>`;
+        let readmeData = 
+`# ${title}
+## Description
+${description}
+## Instructions
+${instructions}
+## Audience
+Intended audience:${userType}
+Should the user of this site need to have a technical background: ${techKnowledge}
+## Technologies Used
+${technologies}
+## Revision History 
+${revHistory}
+## Contribute
+To contribut, please ${contribute}.
+## License
+${license}`;
         writeToFile("readme.md", readmeData);
     });
 };
--- output/readme.md
+++ output/readme.md
@@ -1,25 +1,16 @@
-<!DOCTYPE html>
-        Just a Title
-        Just a Description, 
-            Do some things, 
-                Application Programmer, 
-                    Yes, 
-                        HTML,CSS,JavaScript, 
-                            Initial deployment, 
-                                e-mail me, 
-                                    None
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>week 9 mini project</title>
-        </head>
-        <body>
-        Name: {username} <br>
-        Location: {location} <br>
-        Bio: {bio} <br>
-        <a href="{linkedin}">LinkedIn URL</a> <br>
-        <a href="{github}">GitHub URL</a> <br>
-        </body>
-        </html>
+# application
+## Description
+Just a Description
+## Instructions
+Do some things
+## Audience
+Intended audience:Application Programmer
+Should the user of this site need to have a technical background: Yes
+## Technologies Used
+HTML,CSS,JavaScript
+## Revision History 
+Initial deployment
+## Contribute
+To contribut, please e-mail me.
+## License
+None,MIT License
